f607b089d4c95da0c10960cd03e997ab82112394
fuzhengwei <184172133@qq.com>
2026-06-07 22:05:58 +0800

    feat: 第4-6节：ai-draw 增强，自定义api

--- ai-agent-scaffold-draw-io-app/build.sh
+++ ai-agent-scaffold-draw-io-app/build.sh
@@ -1,7 +1,7 @@
 # 普通镜像构建，随系统版本构建 amd/arm mac 电脑可以选择第2条build方式构建
 #docker build -t fuzhengwei/ai-draw-io-app:1.0 -f ./Dockerfile .
 
-docker build --platform linux/amd64,linux/arm64 --load -t fuzhengwei/ai-draw-io-app:1.6 -f ./Dockerfile .
+docker build --platform linux/amd64,linux/arm64 --load -t fuzhengwei/ai-draw-io-app:1.7 -f ./Dockerfile .
 
 # 兼容 amd、arm 构建镜像
 # docker buildx build --load --platform liunx/amd64,linux/arm64 -t fuzhengwei/ai-agent-scaffold-draw-io-app:1.0 -f ./Dockerfile . --push
